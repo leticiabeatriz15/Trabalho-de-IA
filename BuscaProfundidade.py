cf116655a4a6e2d35b75ce3d2b32dc936c502cb3
Emanuelle <capic.2024118tads0027@aluno.ifpi.edu.br>
2025-06-29 18:05:56 -0300

    Atualizando

--- BuscaProfundidade.py
+++ BuscaProfundidade.py
@@ -2,7 +2,7 @@ from Node import Node
 
 from ArvoreBusca import ArvoreBusca
 
-nodeEstadoInicial = Node([3,1,2,0,4,5,6,7,8], 0, None, None) #Busca 3*3
+nodeEstadoInicial = Node([3,1,2,4,0,5,6,7,8], 0, None, None) #Busca 3*3
 nodeEstadoObjetivo = Node([0,1,2,3,4,5,6,7,8], 0, None, None)
 
 # nodeEstadoInicial = Node([4,1,2,3,0,5,6,7,8,9,10,11,12,13,14,15], 0, None, None) #Busca 4*4
@@ -13,7 +13,6 @@ nodeEstadoObjetivo = Node([0,1,2,3,4,5,6,7,8], 0, None, None)
 
 arvore = ArvoreBusca(nodeEstadoInicial, nodeEstadoObjetivo)
 
-listaNosExplorados = []
 
 def sequenciaAcoes (estadoAtual):
     listaAcoes = []
@@ -29,44 +28,42 @@ def caminhoPercorrido(estadoAtual):
     while estadoAtual.pai is not None:
         caminho.append(estadoAtual.estado)
         estadoAtual = estadoAtual.pai
-        
-        for elemento in caminho:
-            textoOrganizado = ""
-            for i in elemento:
-                if i == 0:
-                    textoOrganizado += "\033[32m"+ str(i) + "\033[0m"
-                else:
-                    textoOrganizado += str(i)
-            
-        print(textoOrganizado)# print (elemento[i])
-    return caminho[::-1] 
+
+    for elemento in caminho[::-1]:
+        textoOrganizado = ""
+        for i in elemento:
+            if i == 0:
+                textoOrganizado += "\033[32m" + str(i) + "\033[0m"
+            else:
+                textoOrganizado += str(i)
+        print(textoOrganizado)
+
+    return caminho[::-1]
       
 def buscaEmProfundidade(problema):
     node = problema
-    
+    listaNosExplorados = set()
     pilha_nos = []
     pilha_nos.append(node)
+    listaNosExplorados.add(tuple(node.estado))  
     
     while pilha_nos:
-        
-        if node.estado in listaNosExplorados:
-            continue
-
-        listaNosExplorados.append(node.estado)
-        print('Nó: ', node.estado)
+        node = pilha_nos.pop()
         
         if arvore.isNoObjetivo(node):
             print("Ações:", sequenciaAcoes(node))
             print("Estado objetivo encontrado!")
             print('Caminho percorrido: ', caminhoPercorrido(node))
-            
-            return sequenciaAcoes(node)
         
-        expande(node, pilha_nos)
-        node = pilha_nos.pop()
+            return listaNosExplorados
+        
+        expande(node, pilha_nos, listaNosExplorados)
+        # print('Nó: ', node.estado)
+
+    return listaNosExplorados
     
       
-def expande(problema, pilha_nos):
+def expande(problema, pilha_nos, listaNosExplorados):
     estado_atual = problema.estado
     indice = estado_atual.index(0)
     tamanhoLista = len(estado_atual)
@@ -74,29 +71,24 @@ def expande(problema, pilha_nos):
     x = indice // raiz
     y = indice % raiz 
 
-    filhos = [] 
 
-    if x > 0:
-        novoIndice = (x - 1) * raiz + y
-        novo_estado = estado_atual[:]
-        novo_estado[indice], novo_estado[novoIndice] = novo_estado[novoIndice], novo_estado[indice]
-        
-        if novo_estado not in listaNosExplorados:
-            pilha_nos.append(Node(novo_estado, 0, "para cima", problema))
+    
 
     if x < raiz - 1:
         novoIndice = (x + 1) * raiz + y
         novo_estado = estado_atual[:]
         novo_estado[indice], novo_estado[novoIndice] = novo_estado[novoIndice], novo_estado[indice]
-        if novo_estado not in listaNosExplorados:
+        if tuple(novo_estado) not in listaNosExplorados:
             pilha_nos.append(Node(novo_estado, 0, "para baixo", problema))
+            listaNosExplorados.add(tuple(novo_estado))
 
     if y > 0:
         novoIndice = x * raiz + (y - 1)
         novo_estado = estado_atual[:]
         novo_estado[indice], novo_estado[novoIndice] = novo_estado[novoIndice], novo_estado[indice]
-        if novo_estado not in listaNosExplorados:
+        if tuple(novo_estado) not in listaNosExplorados:
             pilha_nos.append(Node(novo_estado, 0, "para a esquerda", problema))
+            listaNosExplorados.add(tuple(novo_estado))
 
     
     if y < raiz - 1:
@@ -104,11 +96,19 @@ def expande(problema, pilha_nos):
         novo_estado = estado_atual[:]
         novo_estado[indice], novo_estado[novoIndice] = novo_estado[novoIndice], novo_estado[indice]
         
-        if novo_estado not in listaNosExplorados:
+        if tuple(novo_estado) not in listaNosExplorados:
             pilha_nos.append(Node(novo_estado, 0, "para direita", problema))
+            listaNosExplorados.add(tuple(novo_estado))
 
-    return filhos
+    if x > 0:
+        novoIndice = (x - 1) * raiz + y
+        novo_estado = estado_atual[:]
+        novo_estado[indice], novo_estado[novoIndice] = novo_estado[novoIndice], novo_estado[indice]
+        
+        if tuple(novo_estado) not in listaNosExplorados:
+            pilha_nos.append(Node(novo_estado, 0, "para cima", problema))
+            listaNosExplorados.add(tuple(novo_estado))
 
 
-buscaEmProfundidade(nodeEstadoInicial)
+listaNosExplorados = buscaEmProfundidade(nodeEstadoInicial)
 print("Quantidade de nós explorados:", len(listaNosExplorados))
